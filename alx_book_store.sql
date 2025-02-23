44df97fad7e068fc9fc2cd14e395841866fa6d91
Mohammed Abba Aji <ajinote9@gmail.com>
2025-02-23 03:45:02 -0800

    Added alx book file

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -20,3 +20,19 @@ customer_name VARCHAR(215),
 email VARCHAR(215),
 address TEXT
 );
+
+CREATE TABLE Orders (
+order_id INT AUTO_INCREMENT PRIMARY KEY,
+customer_id INT,
+order_date DATE,
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE Order_Details (
+order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
+order_id INT,
+book_id INT,
+quantity DOUBLE(10,2),
+FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
